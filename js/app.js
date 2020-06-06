299c6018fbf5f3f3b97bc160ed532f067edf1b7c
Lasse Rützou Bruntse <lrbr@itu.dk>
2020-06-06 13:08:14 +0200

    Word titles can be updated

--- js/app.js
+++ js/app.js
@@ -46,6 +46,32 @@
           }
       }
 
+      function wordEditFocus(word){
+        var div = document.getElementById('li_' + word._id);
+        var inputEditWord = document.getElementById('input_title_' + word._id);
+        div.className = 'editing';
+        inputEditWord.focus();
+      }
+
+      function wordUpdate(word, event){
+        console.log(event.target.className);
+        var value = event.target.value.trim();
+        if(!value){
+          db.remove(word);
+        } else{
+        //how to update both fields?
+          word.title = value;
+          db.put(word);
+        }
+      }
+      function enterKeyPressed(word, event) {
+        if (event.keyCode === ENTER_KEY) {
+          var inputEditWord = document.getElementById('input_title_' + word._id);
+          inputEditWord.blur();
+          wordUpdate(word,event);
+        }
+      }
+
       function showWords() {
         db.allDocs({include_docs: true, descending: true}, function(err, doc) {
           redrawTodosUI(doc.rows);
@@ -61,8 +87,7 @@
       }
 
       function createWordListItem(word){
-        var randomColor = Math.floor(Math.random()*16777215).toString(16);
-
+        
             var remove = document.createElement('button');
             remove.className = 'destroy';
             remove.addEventListener('click', removeWord.bind(this, word));
@@ -70,15 +95,32 @@
             var wordLabel = document.createElement('label');
             var definitionLabel = document.createElement('definition');
             wordLabel.appendChild(document.createTextNode(word.title))
+            
             definitionLabel.appendChild(document.createTextNode(word.definition))
-
+            
+            //* input_editWord and input_editDefinition should only appear on toggle */
+            var input_editWord = document.createElement('input');
+            input_editWord.id= 'input_title_' + word._id;
+            input_editWord.className = 'edit';
+            input_editWord.value = word.title;
+            input_editWord.addEventListener('keypress', enterKeyPressed.bind(this, word));
+            // input_editWord.addEventListener('blur', wordUpdate.bind(this, word));
         
+
+            var input_editDefinition = document.createElement('input');
+            input_editDefinition.id= 'input_def_' + word._id;
+            input_editDefinition.className = 'edit';
+            input_editDefinition.value = word.definition;
+          
+            
             var li = document.createElement('li');
             var br = document.createElement('br');
             li.id = 'li_' + word._id;
             
+            
             var divDisplay = document.createElement('div');
             divDisplay.className = 'viewBox';    
+            divDisplay.addEventListener('dblclick', wordEditFocus.bind(this, word));
             
             // divDisplay.style.backgroundColor = "#" + randomColor;
             
@@ -88,6 +130,8 @@
 
             li.appendChild(remove);
             li.appendChild(divDisplay);
+            li.appendChild(input_editWord);
+            li.appendChild(input_editDefinition);
             
             return li;
       }
