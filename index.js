d6600545ba5f2ef5457c20b4bceff280b58fdeb9
Lasse Rützou Bruntse <lrbr@itu.dk>
2020-06-09 22:53:57 +0200

    simple electron app

--- index.js
+++ index.js
@@ -58,6 +58,6 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server spelunking at http://${hostname}:${port}/`);
 });
 
